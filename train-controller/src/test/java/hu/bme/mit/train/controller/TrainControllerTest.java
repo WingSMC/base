15c471828724677772b301a0cfe8b7baf9551310
WingSMC <gergely089@gmail.com>
2022-02-23 18:14:24 +0100

    +TrainControllerTest

--- train-controller/src/test/java/hu/bme/mit/train/controller/TrainControllerTest.java
+++ train-controller/src/test/java/hu/bme/mit/train/controller/TrainControllerTest.java
@@ -1,25 +1,16 @@
-package hu.bme.mit.train.sensor;
+package hu.bme.mit.train.controller;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-
 import hu.bme.mit.train.interfaces.TrainController;
-
-import static org.mockito.Mockito.*;
 
 public class TrainControllerTest {
 
 	@Test
 	public void SpeedLimit_Test() {
-		TrainSystem system = new TrainSystem();
-		TrainController controller = system.getController();
-		TrainSensor sensor = system.getSensor();
-		TrainUser user = system.getUser();
+		TrainController controller = new TrainControllerImpl();
 
 		controller.setSpeedLimit(50);
-
-		Assert.assertEquals(0, controller.getReferenceSpeed());
 
 		controller.setJoystickPosition(50);
 		controller.followSpeed();
@@ -30,5 +21,6 @@ public class TrainControllerTest {
 		controller.setJoystickPosition(5);
 		controller.followSpeed();
 		Assert.assertEquals(10, controller.getReferenceSpeed());
+
 	}
 }
